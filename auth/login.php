249921e9a5f7fbbd8398d4d4e5abc5e8a0058725
hoummadabderrahmane-oss <hoummadabderrahmane@gmail.com>
2026-07-09 16:53:31 +0100

    Create database and project structure

---
+++ auth/login.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CMS Baladiya - Login</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../assets/css/style.css">
+</head>
+<body>
+
+<div class="container vh-100 d-flex justify-content-center align-items-center">
+    <div class="card shadow-lg p-4" style="width:400px;">
+        <h2 class="text-center mb-4">CMS Baladiya</h2>
+
+        <form action="#" method="POST">
+
+            <div class="mb-3">
+                <label>Email</label>
+                <input type="email" class="form-control" placeholder="Enter email">
+            </div>
+
+            <div class="mb-3">
+                <label>Password</label>
+                <input type="password" class="form-control" placeholder="Password">
+            </div>
+
+            <button class="btn btn-primary w-100">
+                Login
+            </button>
+
+        </form>
+
+    </div>
+</div>
+
+</body>
+</html>
---
+++ test.php
@@ -0,0 +1,5 @@
+<?php
+
+require "config/database.php";
+
+echo "<h2 style='color:green'>✅ Database connected successfully!</h2>";
