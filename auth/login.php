e4d759507d523ed3d0bbb58560a4f2f2c61e7ab5
hoummadabderrahmane-oss <hoummadabderrahmane@gmail.com>
2026-07-09 18:26:45 +0100

    Create database and project structure

--- auth/login.php
+++ auth/login.php
@@ -1,20 +1,14 @@
-<?php
-session_start();
-require "../config/database.php";
-
-$message = "";
-
 if(isset($_POST['login'])){
 
     $email = $_POST['email'];
     $password = hash('sha256', $_POST['password']);
 
-    $sql = "SELECT * FROM users WHERE email='$email' AND password='$password'";
-    $result = $conn->query($sql);
+    $stmt = $pdo->prepare("SELECT * FROM users WHERE email = ? AND password = ?");
+    $stmt->execute([$email, $password]);
 
-    if($result->num_rows > 0){
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
-        $user = $result->fetch_assoc();
+    if($user){
 
         $_SESSION['user'] = $user['fullname'];
         $_SESSION['role'] = $user['role'];
@@ -22,96 +16,7 @@ if(isset($_POST['login'])){
         header("Location: ../dashboard/index.php");
         exit();
 
-    }else{
+    } else {
         $message = "Invalid Email or Password";
     }
-
 }
-?>
-
-<!DOCTYPE html>
-<html>
-<head>
-
-<meta charset="UTF-8">
-
-<title>Login</title>
-
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet">
-
-</head>
-
-<body class="bg-light">
-
-<div class="container">
-
-<div class="row justify-content-center mt-5">
-
-<div class="col-md-4">
-
-<div class="card shadow">
-
-<div class="card-header bg-primary text-white">
-
-<h4>CMS Baladiya Login</h4>
-
-</div>
-
-<div class="card-body">
-
-<?php if($message!=""){ ?>
-
-<div class="alert alert-danger">
-<?= $message ?>
-</div>
-
-<?php } ?>
-
-<form method="POST">
-
-<div class="mb-3">
-
-<label>Email</label>
-
-<input
-type="email"
-name="email"
-class="form-control"
-required>
-
-</div>
-
-<div class="mb-3">
-
-<label>Password</label>
-
-<input
-type="password"
-name="password"
-class="form-control"
-required>
-
-</div>
-
-<button
-name="login"
-class="btn btn-primary w-100">
-
-Login
-
-</button>
-
-</form>
-
-</div>
-
-</div>
-
-</div>
-
-</div>
-
-</div>
-
-</body>
-</html>
