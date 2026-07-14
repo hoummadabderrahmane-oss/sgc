7f880b7ee8dd9a9412265ac1360c219158352c64
Abderrahmane Hoummad <hoummadabderrahmane@gmail.com>
2026-07-14 10:20:34 +0100

    c

--- auth/login.php
+++ auth/login.php
@@ -30,7 +30,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $stmt->execute([$username]);
             $user = $stmt->fetch();
             
-            if ($user && password_verify($password, $user['password'])) {
+            if ($user && $password == $user['password'])) {
                 $_SESSION['user_id'] = $user['id'];
                 $_SESSION['username'] = $user['username'];
                 $_SESSION['full_name'] = $user['full_name'];
