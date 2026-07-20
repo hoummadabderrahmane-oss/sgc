448ce33b62c58f84156d6d611804e91f4d0b0021
Abderrahmane Hoummad <hoummadabderrahmane@gmail.com>
2026-07-20 10:15:52 +0100

    Fix footer.php inclusion syntax

--- documents/index.php
+++ documents/index.php
@@ -621,4 +621,4 @@ require_once __DIR__ . '/../includes/header.php';
     </div>
 </div>
 
-<?php require_once __DIR__ . '/../includes/footer.php'; ?>
+<?php require_once __DIR__ . '/../includes/footer.php'; ?> 
